606b8ccf991301b2e94794534c672c30106ca260
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:21:13 +0200

    added constructor

--- src/main/java/com/swingy/model/hero/Deadpool.java
+++ src/main/java/com/swingy/model/hero/Deadpool.java
@@ -1,4 +1,14 @@
 package com.swingy.model.hero;
 
-public class Deadpool {
+public class Deadpool extends Hero{
+
+    public Deadpool() { super(); }
+
+    public Deadpool(String name) {
+        super(name);
+        this.type = "Deadpool";
+        this.attack += 6;
+        this.defense += 1;
+        this.hitPoints += 25;
+    }
 }
