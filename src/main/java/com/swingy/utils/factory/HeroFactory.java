bbc3f06a3d126d0d3f0364eceef277dee086d741
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:23:30 +0200

    added static methods

--- src/main/java/com/swingy/utils/factory/HeroFactory.java
+++ src/main/java/com/swingy/utils/factory/HeroFactory.java
@@ -1,4 +1,39 @@
 package com.swingy.utils.factory;
 
-public class HeroFactory {
+import com.swingy.model.hero.*;
+
+public abstract class HeroFactory {
+    private static Hero newHero;
+    private static Hero newEnemy;
+
+    public static Hero newHero(String name, HeroEnum type) {
+        switch (type) {
+            case DEADPOOL:
+                newHero = new Deadpool(name);
+                break;
+            case THOR:
+                newHero = new Thor(name);
+                break;
+            case WOLVERINE:
+                newHero = new Wolverine(name);
+                break;
+            default:
+                break;
+        }
+        return newHero;
+    }
+
+    public static Hero newEnemy(Hero hero, HeroEnum type) {
+        switch (type) {
+            case MAGNETO:
+                newEnemy = new Magneto(hero.getLevel());
+                break;
+            case ULTRON:
+                newEnemy = new Ultron(hero.getLevel());
+                break;
+            default:
+                break;
+        }
+        return newEnemy;
+    }
 }
