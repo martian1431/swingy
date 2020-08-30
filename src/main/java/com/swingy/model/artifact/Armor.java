747628e4d798ab69aaec6707ab875ced3719e428
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:20:22 +0200

    added constructor and getter

--- src/main/java/com/swingy/model/artifact/Armor.java
+++ src/main/java/com/swingy/model/artifact/Armor.java
@@ -1,4 +1,17 @@
 package com.swingy.model.artifact;
 
-public class Armor {
+public class Armor extends Artifact {
+
+    private int defense;
+
+    public Armor(String name, int defense) {
+        super(name);
+        this.type = ArtifactEnum.ARMOR;
+        this.defense = defense;
+    }
+
+    //   TODO use lombok for getters and setters
+    public int getDefense() {
+        return defense;
+    }
 }
