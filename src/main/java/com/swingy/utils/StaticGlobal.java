86d6de036859368a49bda78bd1d45673ea5feb61
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:27:02 +0200

    added Global variables

--- src/main/java/com/swingy/utils/StaticGlobal.java
+++ src/main/java/com/swingy/utils/StaticGlobal.java
@@ -1,4 +1,24 @@
 package com.swingy.utils;
 
+import com.swingy.model.artifact.Artifact;
+import com.swingy.model.hero.Enemy;
+import com.swingy.model.hero.Hero;
+
 public class StaticGlobal {
+    private StaticGlobal() {
+
+    }
+
+    public static boolean DISPLAY_LOGO = true;
+    public static boolean CONSOLE_MODE = true;
+
+    public static boolean ARTIFACT_DROPPED;
+    public static boolean GOAL_REACHED;
+    public static boolean HERO_RAN;
+
+
+    public static Hero hero;
+    public static Map map;
+    public static Artifact artifact;
+    public static Enemy enemy;
 }
