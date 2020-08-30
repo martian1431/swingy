1282116761b43cd7a2da6edaec1ef4ec0b9bf28b
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:26:17 +0200

    added constructor

--- src/main/java/com/swingy/model/hero/Wolverine.java
+++ src/main/java/com/swingy/model/hero/Wolverine.java
@@ -1,4 +1,14 @@
 package com.swingy.model.hero;
 
-public class Wolverine {
+public class Wolverine extends Hero{
+
+    public Wolverine() { super(); }
+
+    public Wolverine(String name) {
+        super(name);
+        this.type = "Octopus";
+        this.attack += 10;
+        this.defense += 3;
+        this.hitPoints += 75;
+    }
 }
