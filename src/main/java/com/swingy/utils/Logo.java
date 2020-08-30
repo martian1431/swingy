0eef26de1334bb20330ba03b79046e64aba8f777
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 11:17:34 +0200

    customized logo

--- src/main/java/com/swingy/utils/Logo.java
+++ src/main/java/com/swingy/utils/Logo.java
@@ -2,25 +2,42 @@ package com.swingy.utils;
 
 public class Logo {
     public static void displayLogo() {
-        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
-        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "                                                                     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "          Swingy 1.0 We Think Code Developed by pmalope 2019         " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "                                                                     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "       |#|   |#|        |#| |#| |#|      |#|   |#||#|   |#|      |#| " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "    |#|  |#| |#|        |#| |#| |#||#|   |#| |#|    |#|  |#|    |#|  " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "    |#|      |#|        |#| |#| |#| |#|  |#| |#|    |#|   |#|  |#|   " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "      |#|     |#| |#|  |#|  |#| |#|  |#| |#| |#|           |#||#|    " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "        |#|    |#|||#||#|   |#| |#|   |#||#| |#|  |#||#|     |#|     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "    |#|  |#|    |#|  |#|    |#| |#|      |#| |#|    |#|      |#|     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "      |#|       |#|  |#|    |#| |#|      |#|   |#||#|        |#|     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "                                                                     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_GREEN  + "      |#|       |#|  |#|    |#| |#|      |#|      |#|        |#|     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_CYAN   + "      |#|       |#|  |#|    |#| |#|      |#|      |#|        |#|     " + Colors.ANSI_RED + "  ###");
-        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
-        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
+        System.out.println();
+        System.out.println();
+        System.out.println(Colors.ANSI_WHITE  + "                                                                     ");
+        System.out.println(Colors.ANSI_WHITE  + "                                                                     ");
+        System.out.println(Colors.ANSI_PURPLE + "       |*|   |*|        |*| |*| |*|      |*|   |*||*|   |*|      |*| ");
+        System.out.println(Colors.ANSI_PURPLE + "    |*|  |*| |*|        |*| |*| |*||*|   |*| |*|    |*|  |*|    |*|  ");
+        System.out.println(Colors.ANSI_PURPLE + "    |*|      |*|        |*| |*| |*| |*|  |*| |*|    |*|   |*|  |*|   ");
+        System.out.println(Colors.ANSI_YELLOW + "      |*|     |*| |*|  |*|  |*| |*|  |*| |*| |*|           |*||*|    ");
+        System.out.println(Colors.ANSI_BLUE   + "        |*|    |*|||*||*|   |*| |*|   |*||*| |*|  |*||*|     |*|     ");
+        System.out.println(Colors.ANSI_BLUE   + "    |*|  |*|    |*|  |*|    |*| |*|      |*| |*|    |*|      |*|     ");
+        System.out.println(Colors.ANSI_BLUE   + "      |*|       |*|  |*|    |*| |*|      |*|   |*||*|        |*| " + Colors.ANSI_WHITE + "v1.0");
+        System.out.println(Colors.ANSI_WHITE  + "                                                                     ");
+        System.out.println(Colors.ANSI_WHITE  + "                Copyright " + Symbols.COPYRIGHT + " 2020  Made with " + Symbols.HEART + " by pmalope ");
+        System.out.println(Colors.ANSI_WHITE  + "                                                                     ");
+//        S();W();
     }
 
+    public static void S() {
+        System.out.println(Colors.ANSI_RED + "     |*|   ");
+        System.out.println(Colors.ANSI_RED + "  |*|  |*| ");
+        System.out.println(Colors.ANSI_RED + "  |*|      ");
+        System.out.println(Colors.ANSI_RED + "    |*|    ");
+        System.out.println(Colors.ANSI_RED + "      |*|  ");
+        System.out.println(Colors.ANSI_RED + "  |*|  |*| ");
+        System.out.println(Colors.ANSI_RED + "    |*|    ");
+    }
+
+    public static void W() {
+        System.out.print(Colors.ANSI_YELLOW + "  |*|        |*| ");
+        System.out.print(Colors.ANSI_YELLOW + "  |*|        |*| ");
+        System.out.print(Colors.ANSI_YELLOW + "  |*|        |*| ");
+        System.out.print(Colors.ANSI_YELLOW + "   |*| |*|  |*|  ");
+        System.out.print(Colors.ANSI_YELLOW + "    |*|||*||*|   ");
+        System.out.print(Colors.ANSI_YELLOW + "     |*|  |*|    ");
+        System.out.print(Colors.ANSI_YELLOW + "     |*|  |*|    ");
+    }
     public static String logoText = "\n\n\n\n\n\n\nSwingy 1.0 We Think Code Developed by pmalope 2020:"
             +  "\n\nA minimalistic text-based Role Play Game in Java Swing.";
 }
