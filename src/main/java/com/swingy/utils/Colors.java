aca7e007e9fc55b32b2b4f4f29855f6cf075e8fd
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:28:13 +0200

    printing color on console

--- src/main/java/com/swingy/utils/Colors.java
+++ src/main/java/com/swingy/utils/Colors.java
@@ -1,4 +1,20 @@
 package com.swingy.utils;
 
+/**
+ * ANSI Escape Codes for colors Click below link to check them out
+ * on Stackoverflow.
+ *
+ * https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+ *
+ */
 public class Colors {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 }
