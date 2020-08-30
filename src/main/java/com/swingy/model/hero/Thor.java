d94ec91728d139133e0bc39e222848928b7d7ec6
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:26:06 +0200

    added constructor

--- src/main/java/com/swingy/model/hero/Thor.java
+++ src/main/java/com/swingy/model/hero/Thor.java
@@ -1,4 +1,13 @@
 package com.swingy.model.hero;
 
-public class Thor {
+public class Thor extends Hero {
+    public Thor() { super();}
+
+    public Thor(String name) {
+        super(name);
+        this.type = "Thor";
+        this.attack += 8;
+        this.defense += 2;
+        this.hitPoints += 50;
+    }
 }
--- src/main/java/com/swingy/model/hero/Ultron.java
+++ src/main/java/com/swingy/model/hero/Ultron.java
@@ -1,4 +1,13 @@
 package com.swingy.model.hero;
 
-public class Ultron {
+public class Ultron extends Enemy {
+
+    public Ultron(int level) {
+        super(level);
+        this.name = "Lord Geff";
+        this.type = "Ultron";
+        this.attack = level + 5;
+        this.defense = level + 2;
+        this.hitPoints = level + 12;
+    }
 }
