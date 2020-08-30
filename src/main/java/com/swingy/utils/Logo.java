e1b63f9c28fc40fcedd02c5ca982c26f75ca6239
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:24:12 +0200

    added static method display LOGO

--- src/main/java/com/swingy/utils/Logo.java
+++ src/main/java/com/swingy/utils/Logo.java
@@ -1,4 +1,26 @@
 package com.swingy.utils;
 
 public class Logo {
+    public static void displayLogo() {
+        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
+        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "                                                                     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "          Swingy 1.0 We Think Code Developed by pmalope 2019         " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "                                                                     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "       |#|   |#|        |#| |#| |#|      |#|   |#||#|   |#|      |#| " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "    |#|  |#| |#|        |#| |#| |#||#|   |#| |#|    |#|  |#|    |#|  " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "    |#|      |#|        |#| |#| |#| |#|  |#| |#|    |#|   |#|  |#|   " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "      |#|     |#| |#|  |#|  |#| |#|  |#| |#| |#|           |#||#|    " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "        |#|    |#|||#||#|   |#| |#|   |#||#| |#|  |#||#|     |#|     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "    |#|  |#|    |#|  |#|    |#| |#|      |#| |#|    |#|      |#|     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_YELLOW + "      |#|       |#|  |#|    |#| |#|      |#|   |#||#|        |#|     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_WHITE  + "                                                                     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_GREEN  + "      |#|       |#|  |#|    |#| |#|      |#|      |#|        |#|     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "###" + Colors.ANSI_CYAN   + "      |#|       |#|  |#|    |#| |#|      |#|      |#|        |#|     " + Colors.ANSI_RED + "  ###");
+        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
+        System.out.println(Colors.ANSI_RED + "#############################################################################" + Colors.ANSI_RESET);
+    }
+
+    public static String logoText = "\n\n\n\n\n\n\nSwingy 1.0 We Think Code Developed by pmalope 2020:"
+            +  "\n\nA minimalistic text-based Role Play Game in Java Swing.";
 }
