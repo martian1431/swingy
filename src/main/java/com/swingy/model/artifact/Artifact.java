d0efc203c5b6574a0fbce7b8091aed79e51a7d73
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:20:42 +0200

    added constructor and getter

--- src/main/java/com/swingy/model/artifact/Artifact.java
+++ src/main/java/com/swingy/model/artifact/Artifact.java
@@ -1,4 +1,24 @@
 package com.swingy.model.artifact;
 
-public class Artifact {
+import java.io.Serializable;
+
+public abstract class Artifact implements Serializable {
+    private static final long serialVersionUID = 2776303584447042497L;
+
+    String name;
+    ArtifactEnum type;
+
+    Artifact(String name) {
+
+        this.name = name;
+    }
+
+//   TODO use lombok for getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public ArtifactEnum getType() {
+        return type;
+    }
 }
