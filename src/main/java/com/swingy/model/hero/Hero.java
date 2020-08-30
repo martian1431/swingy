fe8e7fc0b672d4dbdb9ee06eed2c13cf4cd80175
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:23:04 +0200

    added constructors, getters, setters, custom methods

--- src/main/java/com/swingy/model/hero/Hero.java
+++ src/main/java/com/swingy/model/hero/Hero.java
@@ -1,4 +1,199 @@
 package com.swingy.model.hero;
 
-public class Hero {
+import com.swingy.model.artifact.*;
+import com.swingy.utils.Map;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import static com.swingy.utils.Colors.ANSI_CYAN;
+import static com.swingy.utils.Colors.ANSI_RESET;
+import static com.swingy.utils.Log.log;
+
+public abstract class Hero {
+    @NotNull
+    @Size(min = 2, max = 30)
+    protected String name;
+
+    protected int level;
+    protected int attack;
+    protected int defense;
+    protected String type;
+    protected int hitPoints;
+    protected int experience;
+    protected int xCoordinate;
+    protected int yCoordinate;
+    protected Armor armor;
+    protected Helm helm;
+    protected Weapon weapon;
+    private Map observer;
+
+    Hero() {
+
+    }
+
+    Hero(String name) {
+        this.name = name;
+        this.level = 1;
+        this.experience = 0;
+
+        Armor defaultArmor = new  Armor("Default Armor", 1);
+        Helm defaultHelm = new Helm("Default Helm", 1);
+        Weapon defaultWeapon = new Weapon("Default Weapon", 1);
+
+        equipHero(defaultArmor, ArtifactEnum.ARMOR);
+        equipHero(defaultHelm, ArtifactEnum.HELM);
+        equipHero(defaultWeapon, ArtifactEnum.WEAPON);
+    }
+
+    public void register(Map map) {
+        observer = map;
+    }
+
+    private void updateMap() {
+        observer.updateHeroPosition();
+    }
+
+    public void setPosition(int x, int y) {
+        this.xCoordinate += x;
+        this.yCoordinate += y;
+        updateMap();
+    }
+
+    public void attack(Hero enemy) {
+        int earnedExperience = 0;
+
+        enemy.defend(this.attack);
+        if (enemy.getHitPoints() <= 0) {
+            if (enemy.getType().equals("Horsefish")) {
+                earnedExperience = (int) (Math.ceil((float)this.level / 2) * 750);
+                this.experience += earnedExperience;
+            } else if (enemy.getType().equals("Jellyfish")) {
+                earnedExperience = (int) (Math.ceil((float)this.level / 2) * 500);
+                this.experience += earnedExperience;
+            }
+            log(ANSI_CYAN + " >>> Well Done, Your Earned " + earnedExperience + "XP" + ANSI_RESET);
+            if (this.experience >= (this.level * 1000 + Math.pow(this.level - 1, 2) * 450)) {
+                levelUp();
+            }
+        }
+    }
+
+    public void defend(int enemyDamage) {
+        int damage = enemyDamage - this.defense;
+
+        if (damage <= 0) {
+            damage = 1;
+        }
+        if (damage > 0) {
+            this.hitPoints -= damage;
+        }
+    }
+
+    /**
+     * Level up the hero.
+     */
+    private void levelUp() {
+        int levelUp;
+
+        this.level++;
+        levelUp = this.level;
+        this.attack += levelUp;
+        this.hitPoints += levelUp;
+        this.defense += 1;
+    }
+
+    public void equipHero(Artifact artifact, ArtifactEnum type) {
+        switch (type) {
+            case ARMOR:
+                if (armor != null) {
+                    defense -= armor.getDefense();
+                } else {
+                    armor = (Armor)artifact;
+                    defense += armor.getDefense();
+                }
+                break;
+            case HELM:
+                if (helm != null) {
+                    hitPoints -= helm.getHitPoints();
+                } else {
+                    helm = (Helm)artifact;
+                    hitPoints += helm.getHitPoints();
+                }
+                break;
+            case WEAPON:
+                if (weapon != null) {
+                    attack -= weapon.getAttack();
+                } else {
+                    weapon = (Weapon)artifact;
+                    attack += weapon.getAttack();
+                }
+                break;
+        }
+    }
+
+    //   TODO use lombok for getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public Armor getArmor() {
+        return armor;
+    }
+
+    public Helm getHelm() {
+        return helm;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public Map getObserver() {
+        return observer;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
 }
