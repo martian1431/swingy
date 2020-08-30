1b12a3738e25191c5811634ae044c8616cbd5730
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:23:44 +0200

    added static method

--- src/main/java/com/swingy/utils/Log.java
+++ src/main/java/com/swingy/utils/Log.java
@@ -1,4 +1,11 @@
 package com.swingy.utils;
 
+import static com.swingy.utils.StaticGlobal.CONSOLE_MODE;
+
 public class Log {
+    public static void log(String message) {
+        if (CONSOLE_MODE == true) {
+            System.out.println(message);
+        }
+    }
 }
