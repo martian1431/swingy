b6ecad13781869b11583730c574632ec3e8e09d2
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 02:15:50 +0200

    blueprint (doesn't compile)

--- src/main/java/com/swingy/Swingy.java
+++ src/main/java/com/swingy/Swingy.java
@@ -1,7 +1,23 @@
 package com.swingy;
 
+import com.swingy.view.console.ConsoleView;
+import com.swingy.view.gui.GameWindow;
+
 public class Swingy {
     public static void main(String[] args) {
-        System.out.println("Hello swingy...");
+        try {
+            String view = args[0];
+            DatabaseHandler.getInstance().setupDatabase("Swingy.db");
+            if (view.toLowerCase().equals("console")) {
+                ConsoleView.run();
+            } else if (view.toLowerCase().equals("gui")) {
+                GameWindow.run();
+            } else {
+                System.err.println("Please choose a view to run...");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+//            FIXME
+            System.out.println("Invalid argument... ");
+        }
     }
 }
