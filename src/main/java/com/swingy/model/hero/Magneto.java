89c9ab361fbb0b67dce9b5b78ba2499210691047
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-30 04:24:28 +0200

    added contructor

--- src/main/java/com/swingy/model/hero/Magneto.java
+++ src/main/java/com/swingy/model/hero/Magneto.java
@@ -1,4 +1,13 @@
 package com.swingy.model.hero;
 
-public class Magneto {
+public class Magneto extends Enemy {
+
+    public Magneto(int level) {
+        super(level);
+        this.name = "The Kid Next Door";
+        this.type = "Magneto";
+        this.attack = level + 6;
+        this.defense = level + 2;
+        this.hitPoints = level + 15;
+    }
 }
