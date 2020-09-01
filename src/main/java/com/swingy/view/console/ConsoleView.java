881b93cb699969c8efd700a7be7c13e721ba33c2
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-01 12:36:35 +0200

    edited methods to display errors properly

--- src/main/java/com/swingy/view/console/ConsoleView.java
+++ src/main/java/com/swingy/view/console/ConsoleView.java
@@ -12,40 +12,27 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 import static com.swingy.utils.Colors.*;
+import static com.swingy.utils.Log.inputSign;
 import static com.swingy.utils.Log.log;
 
 public class ConsoleView {
 
     /** Display menu choices after launching the game. */
     public static void menuOptions() {
-//        boolean validInput = false;
-
-        log(Colors.ANSI_YELLOW + ":::" + Colors.ANSI_RESET + Colors.CYAN_BOLD_BRIGHT + " SELECT YOUR CHOICE " + Colors.ANSI_RESET + Colors.ANSI_YELLOW + ":::\n" + Colors.ANSI_RESET);
+        log(ANSI_YELLOW + ":::" + ANSI_RESET + CYAN_BOLD_BRIGHT + "Select your choice "
+                + ANSI_RESET + ANSI_YELLOW + ":::" + Colors.ANSI_RESET);
         log(ANSI_CYAN + "  1." + ANSI_RESET + " Create A New Hero.");
         log(ANSI_CYAN + "  2." + ANSI_RESET + " Select A Hero.");
         log(ANSI_CYAN + "  3." + ANSI_RESET + " Switch To GUI view.");
-
+        inputSign();
         ConsoleController.menuOption();
-//        do {
-//            if (ConsoleController.menuOptionValidation()) {
-//                validInput = true;
-////                TODO
-//            } else {
-//                log(ANSI_RED + ":::ERROR::: Incorrect choice, please choose between (1-3). Try Again!\n" + ANSI_RESET);
-//                log("");
-//            }
-//        } while (!validInput);
     }
 
 //    TODO
     public static void heroNameOption(CharacterType type) {
-//        boolean validInput = false;
-
-        log(Colors.ANSI_YELLOW + ":::" + Colors.ANSI_RESET + Colors.CYAN_BOLD_BRIGHT + " NAME YOUR HERO " + Colors.ANSI_RESET + Colors.ANSI_YELLOW + ":::\n" + Colors.ANSI_RESET);
+        log(Colors.ANSI_YELLOW + ":::" + Colors.ANSI_RESET + Colors.CYAN_BOLD_BRIGHT + "Name your hero" + Colors.ANSI_RESET + Colors.ANSI_YELLOW + Colors.ANSI_RESET);
+        inputSign();
         ConsoleController.createHero(type);
-//        do {
-//            ConsoleController.createHero(type);
-//        } while (!validInput);
     }
 
     private static void menuOptions(String input) {
@@ -55,45 +42,21 @@ public class ConsoleView {
     public static void heroOptions() {
 //        boolean validInput = false;
 
-        log(Colors.ANSI_YELLOW + ":::" + Colors.ANSI_RESET + Colors.CYAN_BOLD_BRIGHT + " SELECT A HERO TYPE " + Colors.ANSI_RESET + Colors.ANSI_YELLOW + ":::\n" + Colors.ANSI_RESET);
+        log(Colors.ANSI_YELLOW + ":::" + Colors.ANSI_RESET + Colors.CYAN_BOLD_BRIGHT + "Select hero type" + Colors.ANSI_RESET + Colors.ANSI_YELLOW  + Colors.ANSI_RESET);
         log(ANSI_CYAN + "  1." + ANSI_RESET + " Deadpool");
         log(ANSI_CYAN + "  2." + ANSI_RESET + " Thor");
         log(ANSI_CYAN + "  3." + ANSI_RESET + " Wolverine");
+        inputSign();
         ConsoleController.heroType();
-//        do {
-//            if (ConsoleController.heroType()) {
-//                validInput = true;
-//            } else {
-//                log(ANSI_RED + ":::ERROR::: Hero Type Does Not Exist, Try Again!\n" + ANSI_RESET);
-//            }
-//        } while (!validInput);
     }
 
 //    TODO refactor
     /** The menu with cool swingy logo :). */
     public static void welcomeBanner() {
-//        boolean validInput = false;
-
         Logo.displayLogo();
-        log(ANSI_YELLOW + ":::WARNING:::" + "Are ready to go down the Rabit hole? (Y)es or (N)o\n" + ANSI_RESET);
+        log(ANSI_YELLOW + ":::" + ANSI_RESET + CYAN_BOLD_BRIGHT + "Are ready to go down the Rabit hole? (Y)es or (N)o" + ANSI_RESET);
+        inputSign();
         ConsoleController.startGame();
-//        do {
-//            int errorNum = ConsoleController.welcomeOption();
-//            switch (errorNum) {
-//                case 0:
-//                    validInput = true;
-//                    break;
-//                case 1:
-//                    log(ANSI_RED + ":::" + "SCARED?::: Go drink some water and try again later" + ANSI_RESET);
-//                    System.exit(-1);
-//                case -1:
-//                    log(ANSI_RED + ":::ERROR::: Expected input (Y)es or (N)o, Try Again!\n" + ANSI_RESET);
-//                    break;
-//                default:
-//                    break;
-//            }
-//        } while (!validInput);
-//        menuOptions();
     }
 
 //    TODO debug
@@ -104,6 +67,7 @@ public class ConsoleView {
         log(ANSI_RED + "2." + ANSI_CYAN + " East" + ANSI_RESET);
         log(ANSI_RED + "3." + ANSI_CYAN + " South" + ANSI_RESET);
         log(ANSI_RED + "4." + ANSI_CYAN + " West" + ANSI_RESET);
+        inputSign();
     }
 
     /** Display Actions i.e fight or run. */
@@ -111,6 +75,7 @@ public class ConsoleView {
         log(CYAN_BOLD_BRIGHT + ">> Action <<");
         log(ANSI_RED + "1." + ANSI_CYAN + " Fight" + ANSI_RESET);
         log(ANSI_RED + "2." + ANSI_CYAN + " Run" + ANSI_RESET);
+        inputSign();
     }
 
 //    TODO overide
@@ -124,7 +89,6 @@ public class ConsoleView {
         log( " Level " + ANSI_YELLOW + ":" + ANSI_RESET + hero.getLevel() + ANSI_YELLOW + ":" + ANSI_RESET
                 + " Hit Points " + ANSI_YELLOW + ":" + ANSI_RESET + hero.getHitPoints() + ANSI_YELLOW + ":" + ANSI_RESET
                 + " Map size " + ANSI_YELLOW + ":" + ANSI_RESET + mapSize + ANSI_YELLOW + ":" + ANSI_RESET);
-        System.out.println();
     }
 
     public static void existingHero() {
@@ -132,11 +96,10 @@ public class ConsoleView {
     }
 
     public static void heroCount(int count) {
-        if (count == 1) {
+        if (count == 1)
             log(ANSI_YELLOW + count + " Hero Available " + ANSI_RESET);
-        } else {
-            log(count + " Heroes Available " + ANSI_RESET);
-        }
+        log(count + " Heroes Available " + ANSI_RESET);
+        inputSign();
     }
 
     public static void availableHeros(ResultSet heros) throws SQLException {
@@ -146,7 +109,7 @@ public class ConsoleView {
 //            TODO use this
 //            log(":\t" + heros.getString("heroName") + "\t");
         }
-        System.out.println();
-        log(ANSI_YELLOW + ":::" + ANSI_RESET + CYAN_BOLD_BRIGHT + " SELECT YOUR HERO " + ANSI_RESET + ANSI_YELLOW + ":::\n" + ANSI_RESET);
+        log(ANSI_YELLOW + ":::" + ANSI_RESET + CYAN_BOLD_BRIGHT + "Select your hero" + ANSI_RESET + ANSI_YELLOW + ANSI_RESET);
+        inputSign();
     }
 }
