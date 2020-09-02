1032671f45a37e97fefacefe1aeec94fee1a93c8
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-03 01:28:06 +0200

    fixed a bug

--- src/main/java/com/swingy/model/character/heros/Wolverine.java
+++ src/main/java/com/swingy/model/character/heros/Wolverine.java
@@ -8,7 +8,7 @@ public class Wolverine extends Hero {
 
     public Wolverine(String name) {
         super(name);
-        this.type = "Octopus";
+        this.type = "Wolverine";
         this.attack += 10;
         this.defense += 3;
         this.hitPoints += 75;
