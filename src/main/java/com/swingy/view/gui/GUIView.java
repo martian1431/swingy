9fe46ffc5a4576dd36a6a6b17d2ebe0a0af1fee9
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-05 20:14:34 +0200

    new file

---
+++ src/main/java/com/swingy/view/gui/GUIView.java
@@ -0,0 +1,43 @@
+package com.swingy.view.gui;
+
+import com.swingy.controller.GUIController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIView extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    private CardLayout cardLayout;
+
+    public GUIView() {
+        super("Swingy");
+        cardLayout = new CardLayout();
+        GetStarted getStarted = new GetStarted();
+        MainMenu mainMenu = new MainMenu();
+        ExistingHeros existingHeros = new ExistingHeros();
+
+        setLayout(cardLayout);
+
+//        new GUIController(getStarted);
+
+        // adds view to card layout with unique constraints
+        add(getStarted, "Get started");
+        add(mainMenu, "Main Menu");
+        add(existingHeros, "Existing Heros");
+
+//        TODO
+        // switch view according to its constraints on click
+        getStarted.startButton(e -> cardLayout.show(GUIView.this.getContentPane(), "Main Menu"));
+        mainMenu.selectHeroButton(e -> cardLayout.show(GUIView.this.getContentPane(),"Existing Heros"));
+        existingHeros.backButton(e -> cardLayout.show(GUIView.this.getContentPane(), "Main Menu"));
+
+        // frame width & height
+        int FRAME_WIDTH = 1200;
+        int FRAME_HEIGHT = 700;
+        // size of our application frame
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
