fc1eedb9a5a12dba81a18a0f6303de468a5de3d8
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-06 13:38:38 +0200

    added method

--- src/main/java/com/swingy/controller/GUIController.java
+++ src/main/java/com/swingy/controller/GUIController.java
@@ -1,17 +1,29 @@
 package com.swingy.controller;
 
+import com.swingy.view.gui.ExistingHeros;
 import com.swingy.view.gui.GetStarted;
+import com.swingy.view.gui.MainMenu;
 
 public class GUIController {
 
     private GetStarted getStarted;
-
+    private MainMenu mainMenu;
+    private ExistingHeros existingHeros;
 
     public GUIController(GetStarted getStarted) {
         this.getStarted = getStarted;
 
         this.getStarted.startButton(e -> {
             System.out.println("Someone clicke me for no reason");
+        });
+    }
+
+    public GUIController(MainMenu mainMenu) {
+        this.mainMenu = mainMenu;
+
+        this.mainMenu.selectHeroButton(e -> {
+            System.out.println("Get heroes from database");
+//            this.existingHeros.getHeros();
         });
     }
 }
