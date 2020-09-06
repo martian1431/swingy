e56b0f43746f198591ed08da0ca6ca471febaa14
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-06 13:38:06 +0200

    new file

---
+++ src/main/java/com/swingy/view/gui/CreateHero.java
@@ -0,0 +1,36 @@
+package com.swingy.view.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class CreateHero extends JPanel implements ItemListener {
+
+    private JComboBox jComboBox;
+    private JLabel jLabel;
+
+    public CreateHero() {
+        setLayout(new GridBagLayout());
+
+        String s1[] = { "Jalpaiguri", "Mumbai", "Noida", "Kolkata", "New Delhi" };
+
+        jLabel = new JLabel("type your hero name");
+        jComboBox = new JComboBox(s1);
+
+
+        jComboBox.addItemListener(this);
+
+        add(jComboBox, jLabel);
+//        add(jLabel);
+    }
+
+    public void itemStateChanged(ItemEvent e)
+    {
+        // if the state combobox is changed
+        if (e.getSource() == jComboBox) {
+            System.out.println("Okay");
+//            l1.setText(jComboBox.getSelectedItem() + " selected");
+        }
+    }
+}
