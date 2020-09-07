19ae779190eaf4bbaa81f6022fd1389a2231ae96
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-07 09:09:46 +0200

    added static methods

--- src/main/java/com/swingy/model/GameModel.java
+++ src/main/java/com/swingy/model/GameModel.java
@@ -1,27 +1,169 @@
 package com.swingy.model;
 
-import com.swingy.controller.ConsoleController;
 import com.swingy.model.artifact.Armor;
 import com.swingy.model.artifact.Helm;
 import com.swingy.model.artifact.Weapon;
-import com.swingy.model.character.villian.Villian;
+import com.swingy.model.character.heros.Hero;
 import com.swingy.model.character.CharacterType;
-import com.swingy.utils.database.DatabaseWrapper;
+import com.swingy.model.character.villian.Villian;
+import com.swingy.utils.database.Conn;
 import com.swingy.utils.factory.CharacterFactory;
 import com.swingy.utils.factory.MapFactory;
 import com.swingy.view.console.ConsoleView;
 
+import java.io.File;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
-import static com.swingy.controller.ConsoleController.action;
 import static com.swingy.utils.Colors.*;
+import static com.swingy.utils.Globals.*;
+import static com.swingy.utils.Globals.GOAL_REACHED;
+import static com.swingy.utils.Log.inputSign;
 import static com.swingy.utils.Log.log;
-import static com.swingy.utils.StaticGlobal.*;
 
+
+//TODO convert to wrapper
 public class GameModel {
-    
-
+    private static GameModel instance;
+    private static Connection conn = Conn.getConnection();
     private static int[] previousPosition = new int[2];
+
+    private GameModel() {}
+
+    public static GameModel getInstance() {
+        if (instance == null) {
+            instance = new GameModel();
+        }
+        return instance;
+    }
+
+    public void setupDatabase() {
+        createTable();
+    }
+
+    private void createTable() {
+        String sql = String.format("CREATE TABLE IF NOT EXISTS heroes (\nheroID INTEGER PRIMARY KEY," +
+                "\nheroName TEXT NOT NULL UNIQUE ,\nheroClass TEXT NOT NULL ,\nheroLevel INTEGER NOT NULL ," +
+                "\nheroExperience INTEGER NOT NULL ,\nheroHP INTEGER NOT NULL ,\nheroAttack INTEGER NOT NULL ," +
+                "\nheroDefense INTEGER NOT NULL \n );");
+
+        try {
+            //create table
+            Statement stmt = conn.createStatement();
+//            TODO check to see if table was created
+            stmt.executeUpdate(sql);
+            System.out.println("table added");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage() + "\nError: cannot create table");
+            System.exit(-1);
+        }
+    }
+
+    public boolean heroExists(String name) throws SQLException {
+        boolean exists = false;
+        String sql = "SELECT * FROM heroes WHERE heroName='" + name + "'";
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery(sql);
+        if (resultSet.next()) {
+            exists = true;
+        }
+        return exists;
+    }
+
+    public void insertHero(Hero newHero) throws SQLException {
+        String sql = "INSERT INTO heroes (" +
+                "heroName, heroClass, " +
+                "heroLevel, heroExperience, " +
+                "heroHP, heroAttack, heroDefense) " +
+                "VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, newHero.getName());
+        pstmt.setString(2, newHero.getType());
+        pstmt.setInt(3, newHero.getLevel());
+        pstmt.setInt(4, newHero.getExperience());
+        pstmt.setInt(5, newHero.getHitPoints());
+        pstmt.setInt(6, newHero.getAttack());
+        pstmt.setInt(7, newHero.getDefense());
+
+        pstmt.execute();
+    }
+
+    public int numberOfHeroes() throws SQLException {
+        int rowCount = 0;
+        String sql = "SELECT * FROM heroes";
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery(sql);
+        while (resultSet.next())
+            rowCount++;
+        return rowCount;
+    }
+
+    public List<Hero> retrieveAllHeroes() throws SQLException {
+        String sql = "SELECT * FROM heroes";
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet = stmt.executeQuery(sql);
+        return (parseResult(resultSet));
+    }
+
+//    TODO use prepared statement
+    public Hero retrieveHeroData(String name) throws SQLException {
+        String sql = String.format("SELECT * FROM heroes WHERE heroName='%s'", name);
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet =  stmt.executeQuery(sql);
+        Hero hero = parseResult(resultSet).get(0);
+        return hero;
+    }
+
+//    TODO handle resultset validation
+    private List<Hero> parseResult(ResultSet rs) throws SQLException {
+        List<Hero> list = new ArrayList<>();
+        CharacterType type = null;
+        while (rs.next()) {
+            String heroType = rs.getString("heroClass").toUpperCase();
+            switch (heroType) {
+                case "DEADPOOL":
+                    type = CharacterType.DEADPOOL;
+                    break;
+                case "THOR":
+                    type = CharacterType.THOR;
+                    break;
+                case "WOLVERINE":
+                    type = CharacterType.WOLVERINE;
+                    break;
+            }
+            assert type != null;
+            Hero hero = CharacterFactory.newHero(rs.getString("heroName"), type);
+            hero.setId(rs.getInt("heroID"));
+            hero.setAttack(rs.getInt("heroAttack"));
+            hero.setDefense(rs.getInt("heroDefense"));
+            hero.setExperience(rs.getInt("heroExperience"));
+            hero.setHitPoints(rs.getInt("heroHP"));
+            hero.setLevel(rs.getInt("heroLevel"));
+            list.add(hero);
+        }
+        return list;
+    }
+
+//    TODO use prepared statement
+    public void updateHero(Hero hero) throws SQLException {
+//        String sql = "UPDATE heroes SET heroAttack=, heroDefense, "
+        String sql = String.format("UPDATE heroes " +
+                "SET heroAttack='%s', " +
+                "heroDefense='%s', " +
+                "heroExperience='%s', " +
+                "heroHP='%s', " +
+                "heroLevel='%s' WHERE heroName='%s'", hero.getAttack(), hero.getDefense(), hero.getExperience(), hero.getHitPoints(), hero.getLevel(), hero.getName());
+        Statement stmt = conn.createStatement();
+        ResultSet resultSet =  stmt.executeQuery(sql);
+    }
+
+    public List<Hero> retrieveDatabase() {
+        return new ArrayList<Hero>();
+    }
+
 
     public static void moveHero(int direction) {
         switch (direction) {
@@ -54,12 +196,16 @@ public class GameModel {
                 villian = (Villian) CharacterFactory.newEnemy(hero, CharacterType.ULTRON);
             }
             if (CONSOLE_MODE) {
-                ConsoleController.action();
+                action();
             }
         }
     }
 
 
+//    TODO Static methods
+
+
+//    TODO move to base controller
     /**
      * When a character moves to a position occupied by
      * an enemy, the character has two options.
@@ -71,38 +217,39 @@ public class GameModel {
      * of returning to the position. If the odds arent on his side,
      * he must fight the eneny.
      */
-//    public static void action() {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        log(ANSI_YELLOW + "::: You Are Facing: " + villian.getName() + ANSI_RESET);
-//        ConsoleView.showActionOption();
-//        while (scanner.hasNextLine()) {
-//            String input = scanner.nextLine();
-//            int choice = Integer.parseInt(input);
-//
-//            if (choice == 1 || choice == 2) {
-//                switch (choice) {
-//                    case 1:
-//                        fight();
-//                        return;
-//                    case 2:
-//                        run();
-//                        return;
-//                    default:
-//                        break;
-//                }
-//            } else {
-//                //                TODO refactor
-//                log(ANSI_RED + ":::ERROR::: Incorrect choice, please choose between (1-2). Try Again!\n" + ANSI_RESET);
-//                inputSign();
-////                log("Try again!");
-//                ConsoleView.showActionOption();
-//            }
-//        }
-//
-//    }
+    public static void action() {
+        Scanner scanner = new Scanner(System.in);
+
+        log(ANSI_YELLOW + "::: You Are Facing: " + villian.getName() + ANSI_RESET);
+        ConsoleView.showActionOption();
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            int choice = Integer.parseInt(input);
+
+            if (choice == 1 || choice == 2) {
+                switch (choice) {
+                    case 1:
+                        fight();
+                        return;
+                    case 2:
+                        run();
+                        return;
+                    default:
+                        break;
+                }
+            } else {
+                //                TODO refactor
+                log(ANSI_RED + ":::ERROR::: Incorrect choice, please choose between (1-2). Try Again!\n" + ANSI_RESET);
+                inputSign();
+//                log("Try again!");
+                ConsoleView.showActionOption();
+            }
+        }
+
+    }
 
 
+//    TODO move to model base controller
     /**
      * Engages the Hero in a battle with
      * the enemy. If the character ran and the odds
@@ -133,7 +280,7 @@ public class GameModel {
             }
         } else {
             try {
-                DatabaseWrapper.getInstance().updateHero(hero);
+                GameModel.getInstance().updateHero(hero);
                 hero.setPosition(0, 0);
                 battleGains();
                 log(ANSI_GREEN + ":::" + "Congratulations, You Won The Battle!" + ANSI_RESET);
@@ -146,6 +293,7 @@ public class GameModel {
         }
     }
 
+//    TODO move to base controller
     /**
      * Gives the character a 50% chance of returning to
      * the previous position. If the odds arent on
@@ -166,6 +314,7 @@ public class GameModel {
     }
 
 
+//    TODO move to model
     /**
      * If character wins a battle, he gains:
      * 1. Experience points, based on the enemy power.
@@ -203,7 +352,7 @@ public class GameModel {
                     return;
                 }
                 // Equip the character.
-                ConsoleController.equip(artifactType);
+                equip(artifactType);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
@@ -211,6 +360,41 @@ public class GameModel {
             log( ANSI_RED + ":::Sorry, No Artifact Dropped!");
         }
     }
+
+
+//    TODO move to model
+    /**
+     * Equip the character with the chosen artifact.
+     * The character wont be equiped if the Experience
+     * is chosen instead of an Artifact.
+     *
+     * @param artifactType The artifact type.
+     */
+    private static void equip(String artifactType) {
+        if (CONSOLE_MODE) {
+            Scanner scanner = new Scanner(System.in);
+            log(ANSI_YELLOW + "::: Do You Wanna Keep The Artifact?\n1. YES!\n2. NO!" + ANSI_RESET);
+            inputSign();
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                if (input.equals("1") || input.equals("2")) {
+                    int choice = Integer.parseInt(input.trim());
+                    if (choice == 1) {
+                        hero.equipHero(artifact, artifact.getType());
+                        log("::: " + hero.getName() + " Is Equipped With " + artifactType);
+                        break;
+                    } else if (choice == 2) {
+                        break;
+                    }
+                } else {
+                    //                TODO refactor
+                    log(ANSI_RED + ":::ERROR::: Incorrect Choice, Try Again!" + ANSI_RESET);
+                    inputSign();
+                }
+            }
+        }
+    }
+
 
 //    TODO move to model
     /**
