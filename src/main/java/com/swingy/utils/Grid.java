5ea75bdb66ae91738b10f09bd3a50768a0fdfc33
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-08 20:38:18 +0200

    using lombok for getter and setter

--- src/main/java/com/swingy/utils/Grid.java
+++ src/main/java/com/swingy/utils/Grid.java
@@ -1,39 +1,27 @@
 package com.swingy.utils;
 
 import com.swingy.model.character.heros.Hero;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Random;
 
 import static com.swingy.utils.Globals.CONSOLE_MODE;
 import static com.swingy.utils.Globals.hero;
 
+@Getter
+@Setter
 public class Grid {
-    private Hero heroObj; //TODO player
+    private Hero heroObj;
     private char[][] map;
     private int size;
     private int[] previousPosition = new int[] {-1, -1};
 
-    /**
-     * SwingyMap constructor: initialize the {@code size} with
-     * the parameter {@code size} and give the map a size. Since the map is
-     * a square, the number of rows is the same as the number
-     * of columns, hence: {@code this.map = new char[size][size];}
-     *
-     * @param size The size of the column/row.
-     */
     public Grid(int size) {
         this.size = size;
         this.map = new char[size][size];
     }
 
-    /**
-     * Registers the character to the map and set
-     * the initial position of the character(H) as the
-     * center of the map. The character(H) will always
-     * start at the center of the map.
-     *
-     * @param hero The character to be registered.
-     */
     public void registerHero(Hero hero) {
 
         this.heroObj = hero;
@@ -46,13 +34,6 @@ public class Grid {
         this.map[size / 2][size / 2] = 'H';
     }
 
-    /**
-     * Updates the position of the character, sets the previous position to '.'
-     * character and set the current coordinates to the previous position
-     * coordinates. If the character(H) gets to an enemy(E)'s position, set 'X'
-     * to show that the Hero(H) came accross an enemy(E) and then display
-     * the updated positions on the map.
-     */
     public void updateHeroPosition() {
         this.map[previousPosition[0]][previousPosition[1]] = '.';
         previousPosition[0] = hero.getXCoordinate();
@@ -68,11 +49,6 @@ public class Grid {
         }
     }
 
-    /**
-     * When a map is generated, villians of
-     * varying power are spread randomly over
-     * the map.
-     */
     public void spreadEnemies() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -89,11 +65,7 @@ public class Grid {
         }
     }
 
-    /**
-     * Dipslay the current position of the
-     * randomly spred villians and the character
-     * on the map with some cool colors.
-     */
+
     public void displayMap() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -114,18 +86,5 @@ public class Grid {
             }
             System.out.println("\n");
         }
-    }
-
-    //   TODO use lombok for getters and setters
-    public char[][] getMap() {
-        return map;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public int[] getPreviousPosition() {
-        return previousPosition;
     }
 }
