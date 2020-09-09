f1110b8b8b4d265eda15bcf9ca60fd03ba125cce
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-09 14:32:58 +0200

    made method public

--- src/main/java/com/swingy/controller/ConsoleController.java
+++ src/main/java/com/swingy/controller/ConsoleController.java
@@ -187,7 +187,7 @@ public class ConsoleController {
     }
 
     @NotNull
-    private static Scanner getScanner() {
+    public static Scanner getScanner() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("$ ");
         return scanner;
