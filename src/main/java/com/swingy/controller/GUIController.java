e0dde27973932ef4e19d468138d46c45982445f9
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-10 17:21:42 +0200

    overloading constructor

--- src/main/java/com/swingy/controller/GUIController.java
+++ src/main/java/com/swingy/controller/GUIController.java
@@ -1,5 +1,6 @@
 package com.swingy.controller;
 
+import com.swingy.view.gui.CreateHero;
 import com.swingy.view.gui.ExistingHeros;
 import com.swingy.view.gui.GetStarted;
 import com.swingy.view.gui.MainMenu;
@@ -9,6 +10,7 @@ public class GUIController {
     private GetStarted getStarted;
     private MainMenu mainMenu;
     private ExistingHeros existingHeros;
+    private CreateHero createHero;
 
     public GUIController(GetStarted getStarted) {
         this.getStarted = getStarted;
@@ -25,6 +27,21 @@ public class GUIController {
         this.mainMenu.selectHeroButton(e -> {
             System.out.println("Get heroes from database");
             this.existingHeros.getHeros();
+        });
+    }
+
+    public GUIController(CreateHero createHero) {
+        this.createHero = createHero;
+
+        this.createHero.createHero(e -> {
+            String heroName = this.createHero.getHeroName();
+            String heroType = this.createHero.getHeroType();
+
+//            TODO validate input
+//            TODO create hero
+//            TODO generate grid
+            System.out.println("Hero Name -> " + heroName);
+            System.out.println("Hero type -> " + heroType);
         });
     }
 }
